83c1360f489b36cf74f411cb10d644fce4f2a8a8
Nischal-Ad <92177440+Nischal-Ad@users.noreply.github.com>
2024-02-24 08:37:12 +0545

    navigation error fix

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,9 @@ export default {
     theme: {
         extend: {
             colors: {
-                "custom-black": "#333333",
+                "custom-black": "#031926",
+                "custom-green": "#459D30",
+                "custom-white": "#F4F4F4",
             },
             fontFamily: {
                 Montserrat: ["Montserrat"],
